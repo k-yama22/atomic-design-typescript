b119f3f712a60905e3a85cce1dbbf9b34b600a4f
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-04 23:54:39 +0900

    ⚡️ pagesフォルダを作成し、ユーザー画面とトップページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
-import {BrowserRouter} from "react-router-dom";
 import React from 'react';
 import './App.css';
-import { PrimaryButton } from './components/atoms/button/PrimaryButton';
-import { SecondaryButton } from './components/atoms/button/SecondaryButton';
-import { SearchInput } from './components/molecules/SearchInput';
-import { UserCard } from './components/organisms/user/UserCard';
-import { HeaderOnly } from './components/templates/HeaderOnly';
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-
-const user = {
-    name: "yama",
-    image: "https://source.unsplash.com/NE0XGVKTmcA",
-    email: "12345@eee.ee.ee",
-    phone: "090-0000-0000",
-    company: {
-      name: "テスト会社"
-    },
-    website: "https://google.com"
-}
+import { Router } from "./router/Router";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router/>
 }
 
 export default App;
