2b647622703a56420b07f09e14c851084c60d3fc
k-yama22 <ymgt.katsuya.0322@gmail.com>
2021-11-09 22:57:04 +0900

    ⚡️ propsに対する型定義について

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Router } from "./router/Router";
 
-function App() {
-  return <Router/>
+interface AppProp {
+  message: string;
 }
+
+const App = ({ message }: AppProp) => {
+  return (
+    <>
+      <div>{message}</div>
+      <Router />;
+    </>
+  );
+};
 
 export default App;
